678fc129fef3429c1ad87d83627a0ee32d83041c
edwd_nicholas <hiraga@lycos.co.kr>
2013-03-18 16:47:01 +0900

    verion2

--- attributes/ceilometer_conf.rb
+++ attributes/ceilometer_conf.rb
@@ -1,18 +1,47 @@
-default['ceilometer_dir']      = "/opt/ceilometer"
-default['ceilometer_conf_dir'] = "/etc/ceilometer"
-default['ceilometer_conf']     = "/etc/ceilometer/ceilometer.conf"
-default['ceilometer_log_dir']  = "/var/log/ceilometer"
-default['install_record']      = "install_time_record_log"
-default['user']                = "root"
-default['group']               = "root"
-default['nova_conf']           = "/etc/nova/nova.conf"
-default['swift_conf_dir']      = "/etc/swift"
-default['swift_conf']          = "/etc/swift/proxy-server.conf"
-default['use_swift']           = "yes"
-default['env']                 = "/home/test/conf"
-default['os_username']         = "admin"
-default['os_tenant_name']      = "admin"
-default['os_password']         = "13245"
-default['os_auth_url']         = "http://127.0.0.1:5000/v2.0"
+# ceilometer setting
+default['ceilometer']['ceilometer_dir']       = "/opt/ceilometer"
+default['ceilometer']['ceilometer_conf_dir']  = "/etc/ceilometer"
+default['ceilometer']['ceilometer_conf']      ="/etc/ceilometer/ceilometer.conf"
+default['ceilometer']['ceilometer_log_dir']   = "/var/log/ceilometer"
 
+# user group to be used for install
+default['ceilometer']['user']                 = "root"
+default['ceilometer']['group']                = "root"
+
+# swift setting
+default['ceilometer']['swift_conf_dir']       = "/etc/swift"
+default['ceilometer']['swift_conf']           = "/etc/swift/proxy-server.conf"
+default['ceilometer']['use_swift']            = "yes"
+
+# environment setting
+default['ceilometer']['os_username']          = "admin"
+default['ceilometer']['os_tenant_name']       = "admin"
+default['ceilometer']['os_password']          = "password"
+default['ceilometer']['os_auth_url']          = "http://14.63.205.39:5000/v2.0"
+
+# nova setting
+default['ceilometer']['nova_dir']             = "/opt/nova"
+default['ceilometer']['nova_conf_dir']        = "/etc/nova"
+default['ceilometer']['nova_conf']            = "/etc/nova/nova.conf"
+
+# keystone setting
+default['ceilometer']['keystone_dir']         = "/opt/keystone"
+default['ceilometer']['keystone_conf_dir']    = "/etc/keystone"
+default['ceilometer']['keystone_conf']        = "/etc/keystone/keystone.conf"
+
+# mysql setting : mysql://nova:password@14.63.205.38/nova
+default['ceilometer']['mysql_db']['nova_user']= "nova"
+default['ceilometer']['mysql_db']['nova_database_name'] = "nova"
+default['ceilometer']['mysql_db']['password'] = "password"
+
+default['ceilometer']['mysql_db']['keystone_user']= "keystone"
+default['ceilometer']['mysql_db']['keystone_database_name'] = "keystone"
+default['ceilometer']['mysql_db']['password'] = "password"
+
+
+# mongodb setting: mongodb://127.0.0.1:27017/ceilometer
+default['ceilometer']['mongo_db']['dbs_name'] = "ceilometer"
+default['ceilometer']['mongo_db']['user']     = nil
+default['ceilometer']['mongo_db']['password'] = nil
+default['ceilometer']['mongo_db']['scheme']   = "mongodb"
 
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,15 @@
-name             'ceilometer cookbook'
-maintainer       'NG'
+name             'ceilometer'
+maintainer       'ng'
 maintainer_email 'email@kt.com'
-license          'apache 2.0'
-description      'Installs/Configures ceilometer_test'
+license          'All rights reserved'
+description      'Installs/Configures ceilometer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-recipe            "default", "module install recipe"
+%w{ ubuntu }.each do |os|
+  supports os
+end
 
+%w{ osops }.each do |dep|
+  depends dep
+end
 
